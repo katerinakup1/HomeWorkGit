fb0d89ff2459e5485d181a4a77bab406cc3790b3
katerinakup1 <42445968+katerinakup1@users.noreply.github.com>
2018-08-27 21:06:12 +0300

    Add files via upload

--- GitMap.java
+++ GitMap.java
@@ -5,5 +5,18 @@ import java.util.HashMap;
 public class GitMap {
     public static void main(String[] args) {
         HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1, "один");
+        hashMap.put(2, "два");
+        hashMap.put(3, "три");
+        hashMap.put(4, "четыре");
+        hashMap.put(5, "пять");
+        hashMap.put(6, "шесть");
+        hashMap.put(7, "семь");
+        hashMap.put(8, "восемь");
+        hashMap.put(9, "девять");
+        hashMap.put(10, "десять");
+        hashMap.put(11, "одиннадцать");
+        hashMap.put(12, "двенадцать");
+        hashMap.put(13, "тринадцать");
     }
 }
