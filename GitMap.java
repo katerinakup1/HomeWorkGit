6e94df71725d7ffc5bc958a61e53223862ce7aa5
katerinakup1 <42445968+katerinakup1@users.noreply.github.com>
2018-08-27 21:05:06 +0300

    Add files via upload

--- GitMap.java
+++ GitMap.java
@@ -1,6 +1,9 @@
 package Git;
 
+import java.util.HashMap;
+
 public class GitMap {
     public static void main(String[] args) {
+        HashMap<Integer, String> hashMap = new HashMap<>();
     }
 }
